f58fc4f64622eeed60176f3d1a3cc9e9776f7171
Willian Nunes de Oliveira <williangd@yahoo.com.br>
2016-09-29 11:31:14 -0300

    Separado js do html

--- calcv2.js
+++
@@ -1,7 +0,0 @@
-angular.module('CalculatorApp', [])
-    .controller('CalculatorController', function($scope) {
-        $scope.result = function() {
-			$teste = Math.round((((($scope.VLPRECO * $scope.VLINDICEFINANCEIRO) * $scope.VLINDICETABELAPRECO)) * $scope.VLINDICECLIENTE) * 100 )/100;
-			return $teste * $scope.QTD;
-        };
-    });
